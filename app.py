b105f70df38af3f348903e4522e3ff68527252e0
gddev <dev.gustavodatovo@outlook.com>
2025-02-28 15:38:08 -0300

    Saving 404 progress before changing branches

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template  # type: ignore
 
 app = Flask(__name__)
 
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET',])
 def get_index():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def get_404(e):
+    return render_template('404.html')
