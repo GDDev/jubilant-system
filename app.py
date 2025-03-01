fe01e342244bdeed4f135674cc13cd6e550638a1
gddev <dev.gustavodatovo@outlook.com>
2025-03-01 15:48:49 -0300

    improving general layout and untying 404.html from layout.html

--- app.py
+++ app.py
@@ -10,6 +10,26 @@ def get_index():
     return render_template('index.html')
 
 
+@app.route('/diets', methods=['GET',])
+def get_user_diets():
+    return render_template('user_diets.html')
+
+
+@app.route('/workouts', methods=['GET',])
+def get_user_workouts():
+    return render_template('user_workouts.html')
+
+
+@app.route('/chats', methods=['GET',])
+def get_chats():
+    return render_template('chats.html')
+
+
+@app.route('/notifications', methods=['GET',])
+def get_notifications():
+    return render_template('notifications.html')
+
+
 @app.route('/generate_quote', methods=['GET',])
 def get_quote():
     return jsonify(choose_quote())
