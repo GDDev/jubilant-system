2bff0a4ab2cf544d8404fee31d957bec88d6a8e5
gddev <dev.gustavodatovo@outlook.com>
2025-03-01 19:29:51 -0300

    I believe I refactored it to be an actual API now, at least it still works...

---
+++ api_quotes.py
@@ -0,0 +1,15 @@
+from flask import jsonify, make_response
+from flask_restful import Resource
+
+from not_found import choose_quote
+
+
+class NotFoundPageQuoteApi(Resource):
+    def __init__(self):
+        pass
+
+    def get(self):
+        quote = choose_quote()
+        response = make_response(jsonify(quote))
+        response.set_cookie('used_quote', quote['quote'])
+        return response
