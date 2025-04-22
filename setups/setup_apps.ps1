c6d6093bc68866b79d686dc6c0f2aece6c2b115c
gddev <dev.gustavodatovo@outlook.com>
2025-04-22 20:10:02 -0300

    Updated setup_apps.ps1

--- setups/setup_apps.ps1
+++ setups/setup_apps.ps1
@@ -4,4 +4,4 @@ Write-Host "Iniciando Not_Found..."
 Start-Process -NoNewWindow -FilePath "python" -ArgumentList "..\not_found\app.py"
 
 Write-Host "Iniciando aplicacao..."
-Start-Process -NoNewWindow -FilePath "python" -ArgumentList "..\app\app.py"
+Start-Process -NoNewWindow -FilePath "python" -ArgumentList "..\project\app.py"
