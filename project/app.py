96855da14249a58edd9c23ddaf7fd931b9d16f94
gddev <dev.gustavodatovo@outlook.com>
2025-04-26 00:49:16 -0300

    Fixing setup_config_file.ps1

--- project/app.py
+++ project/app.py
@@ -1,57 +1,64 @@
-from datetime import timedelta
-
 import requests
 from flask import Flask, render_template, request, url_for, redirect
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
-
 from core import db, migrate
 
 
+def config_flask() -> Flask:
+    flask = Flask(__name__)
+    flask.config.from_pyfile('.settings.py', silent=True)
+    return flask
+
+def init_db(flask: Flask):
+    db.init_app(flask)
+    migrate.init_app(flask, db)
+
+def register_all_bp(flask: Flask):
+    from project.auth import auth
+    from project.main import main
+    from project.user import user, profile
+
+    flask.register_blueprint(auth, url_prefix='/auth')
+    flask.register_blueprint(main, url_prefix='/')
+    flask.register_blueprint(user, url_prefix='/usuario')
+    flask.register_blueprint(profile, url_prefix='/perfil')
+
+def config_login_manager(flask: Flask):
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.signin'
+    login_manager.refresh_view = 'auth.refresh'
+    login_manager.needs_refresh_message = 'Por favor, confirme sua senha para continuar.'
+    login_manager.init_app(flask)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        from project.user import UserProfile
+        try:
+            return db.session.query(UserProfile).filter_by(alt_id=user_id).first()
+        except (ValueError, TypeError):
+            return None
+
+    @login_manager.unauthorized_handler
+    def unauthorized():
+        if request.args.get('next') is None:
+            return redirect(url_for('auth.signin'))
+        return render_template('unauthorized.html'), 401
+
 def create_app() -> Flask:
-    app = Flask(__name__)
-    app.config.from_pyfile('.settings.cfg', silent=True)
+    flask = config_flask()
 
-    db.init_app(app)
-    migrate.init_app(app, db)
+    init_db(flask)
 
-    csrf = CSRFProtect(app)
+    csrf = CSRFProtect(flask)
+    with flask.app_context():
+        register_all_bp(flask)
+        config_login_manager(flask)
 
-    with app.app_context():
-        from project.auth import auth
-        from project.main import main
-        from project.user import user, profile
-        app.register_blueprint(auth, url_prefix='/auth')
-        app.register_blueprint(main, url_prefix='/')
-        app.register_blueprint(user, url_prefix='/usuario')
-        app.register_blueprint(profile, url_prefix='/perfil')
+    return flask
 
-        login_manager = LoginManager()
-        login_manager.login_view = 'auth.signin'
-        login_manager.refresh_view = 'auth.refresh'
-        login_manager.needs_refresh_message = 'Por favor, confirme sua senha para continuar.'
-        login_manager.init_app(app)
-
-        @login_manager.user_loader
-        def load_user(user_id):
-            from project.user import UserProfile
-            try:
-                return db.session.query(UserProfile).filter_by(alt_id=user_id).first()
-            except (ValueError, TypeError):
-                return None
-
-        @login_manager.unauthorized_handler
-        def unauthorized():
-            if request.args.get('next') is None:
-                return redirect(url_for('auth.signin'))
-            return render_template('unauthorized.html'), 401
-
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
-
-    @app.errorhandler(404)
+def config_error_handlers(flask: Flask):
+    @flask.errorhandler(404)
     def not_found(_):
         response = requests.get(
             'http://127.0.0.1:9090/not_found/generate_quote',
@@ -60,10 +67,11 @@ if __name__ == '__main__':
         quote = response.json()
         return render_template('not_found.html', quote=quote)
 
-    @app.errorhandler(403)
+    @flask.errorhandler(403)
     def forbidden(_):
         return render_template('forbidden.html'), 403
 
-
+if __name__ == '__main__':
+    app = create_app()
+    config_error_handlers(app)
     app.run(host='127.0.0.1', port=8000, debug=True)
-
--- setups/setup_config_file.ps1
+++ setups/setup_config_file.ps1
@@ -6,15 +6,15 @@ if (!(Test-Path $configPath)) {
     New-Item -Path $configPath -ItemType "file"
 
     $lines = @(
-        "import os"
-        "DB_TYPE = os.getenv('FLASK_DB_TYPE')"
-        "DB_USER = os.getenv('FLASK_DB_USER')"
-        "DB_PWD = os.getenv('FLASK_DB_PWD')"
-        "DB_HOST = os.getenv('FLASK_DB_HOST')"
-        "DB_PORT = os.getenv('FLASK_DB_PORT')"
-        "DB_NAME = os.getenv('FLASK_DB_NAME')"
+        "from os import getenv"
+        "DB_TYPE = getenv('FLASK_DB_TYPE')"
+        "DB_USER = getenv('FLASK_DB_USER')"
+        "DB_PWD = getenv('FLASK_DB_PWD') or ''"
+        "DB_HOST = getenv('FLASK_DB_HOST')"
+        "DB_PORT = getenv('FLASK_DB_PORT')"
+        "DB_NAME = getenv('FLASK_DB_NAME')"
 
-        "SECRET_KEY = os.getenv('FLASK_SECRET_KEY')",
+        "SECRET_KEY = getenv('FLASK_SECRET_KEY')",
         "SQLALCHEMY_DATABASE_URI = f'{DB_TYPE}://{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'"
     )
 
