62227e3fbc07c4494c8e625daff8eb3006c22280
gddev <dev.gustavodatovo@outlook.com>
2025-05-14 14:20:59 -0300

    Exposing 'app' so that gunicorn can find it

--- project/app.py
+++ project/app.py
@@ -1,6 +1,7 @@
 from . import create_app, config_error_handlers
 
+app = create_app()
+config_error_handlers(app)
+
 if __name__ == '__main__':
-    app = create_app()
-    config_error_handlers(app)
     app.run(host='127.0.0.1', port=8000, debug=True)
