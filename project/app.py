31674542db6fee766546f94402f80317d3330205
gddev <dev.gustavodatovo@outlook.com>
2025-06-03 22:17:18 -0300

    Kept searchbar on feed and friend list only

--- project/app.py
+++ project/app.py
@@ -5,8 +5,8 @@ from . import create_app, config_error_handlers
 
 app = create_app()
 config_error_handlers(app)
-if not os.environ.get("WERKZEUG_RUN_MAIN"):
-    mock_100_users(app)
+# if not os.environ.get("WERKZEUG_RUN_MAIN"):
+#     mock_100_users(app)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=9090, debug=True)
