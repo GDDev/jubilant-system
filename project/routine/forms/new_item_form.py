d6980dcbbdb41964df03b61192b372a07737a3f8
gddev <dev.gustavodatovo@outlook.com>
2025-06-06 21:18:08 -0300

    Adding date validations

--- project/routine/forms/new_item_form.py
+++ project/routine/forms/new_item_form.py
@@ -1,10 +1,17 @@
+import datetime
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, DateField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, ValidationError
+
+
+def not_a_past_date(form, field):
+    if field.data <= datetime.date.today():
+        raise ValidationError('A data não pode ser no passado.')
 
 
 class NewItemForm(FlaskForm):
     name = StringField('Nome', validators=[DataRequired(), Length(max=50)])
-    expiration_date = DateField('Expirar em', format='%Y-%m-%d', validators=[Optional()])
+    expiration_date = DateField('Expirar em', format='%Y-%m-%d', validators=[Optional(), not_a_past_date])
     source = TextAreaField('Referências', validators=[Length(max=500, message='Limite de 500 caracteres')])
     submit = SubmitField('Salvar e continuar', render_kw={'class': 'btn btn-outline-success'})
