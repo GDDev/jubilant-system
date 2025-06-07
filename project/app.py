832b6c7e3c36ad28f7c8a9cd7c279a5056a134f5
Gustavo Datovo <94084352+GDDev@users.noreply.github.com>
2025-06-06 22:25:45 -0300

    Uncommenting to populate the db

--- project/app.py
+++ project/app.py
@@ -5,9 +5,9 @@ from . import create_app, config_error_handlers
 
 app = create_app()
 config_error_handlers(app)
-# if not os.environ.get("WERKZEUG_RUN_MAIN"):
-#     mock_100_users(app)
-#     populate_majors(app)
+if not os.environ.get("WERKZEUG_RUN_MAIN"):
+    mock_100_users(app)
+    populate_majors(app)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=9090, debug=True)
