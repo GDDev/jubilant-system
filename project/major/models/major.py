62c49f72b0f9420a03deed276c77b42dd8f85f21
gddev <dev.gustavodatovo@outlook.com>
2025-06-06 22:09:18 -0300

    Fix Major field sizes

--- project/major/models/major.py
+++ project/major/models/major.py
@@ -20,11 +20,11 @@ class Shift(str, Enum):
 
 
 class MajorMixin:
-    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
     level: Mapped[str] = mapped_column(String(50), nullable=False)
     university: Mapped[str] = mapped_column(String(50), nullable=False)
     uni_acronym: Mapped[str] = mapped_column(String(5), nullable=True)
-    area_tag: Mapped[str] = mapped_column(String(36), nullable=False)
+    area_tag: Mapped[str] = mapped_column(String(50), nullable=False)
     shift: Mapped[Shift] = mapped_column('shift', nullable=True, default=Shift.MORNING)
     min_semesters: Mapped[int] = mapped_column(Integer, nullable=True, default=3)
     max_semesters: Mapped[int] = mapped_column(Integer, nullable=True, default=16)
